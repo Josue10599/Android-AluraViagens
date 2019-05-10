ad8abf5b44621256b4ae0332d05dfa29b74bd20e
Josue Lopes <josue10599@gmail.com>
2019-05-10 14:37:01 -0300

    Created ResumoCompraActivity

---
+++ app/src/main/java/com/fulltime/aluraviagens/ui/activity/ResumoCompraActivity.java
@@ -0,0 +1,15 @@
+package com.fulltime.aluraviagens.ui.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.fulltime.aluraviagens.R;
+
+public class ResumoCompraActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_resumo_compra);
+    }
+}
